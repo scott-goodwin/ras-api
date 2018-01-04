61a42d9cd35fdd31115130c1da99b3c0504f3c7d
Ramin <ramin.esfan@gmail.com>
2018-01-04 16:25:41 +0000

    quick-fix

--- app/uk/gov/hmrc/rasapi/services/SessionCacheService.scala
+++ app/uk/gov/hmrc/rasapi/services/SessionCacheService.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 HM Revenue & Customs
+ * Copyright 2018 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -28,11 +28,11 @@ trait SessionCacheService {
 
   val sessionCache: ShortLivedHttpCaching = RasShortLivedHttpCaching
   private val source = "ras"
-  private val cacheId = "fileSession"
+  private val formId = "fileSession"
   def updateFileSession(userId : String, userFile:CallbackData, resultsFile:Option[ResultsFileMetaData])(implicit hc: HeaderCarrier) = {
-    sessionCache.fetchAndGetEntry[FileSession](source,cacheId,userId).flatMap{ session =>
 
-    sessionCache.cache[FileSession](source,cacheId,userId,
+    sessionCache.fetchAndGetEntry[FileSession](source,userId,formId).flatMap{ session =>
+    sessionCache.cache[FileSession](source,userId,formId,
       FileSession(Some(userFile), resultsFile, userId, session.get.uploadTimeStamp) ).recover {
         case ex: Throwable => Logger.error(s"unable to save FileSession to cache => " +
           s"${userId} , userFile : ${userFile.toString} , resultsFile id : " +
