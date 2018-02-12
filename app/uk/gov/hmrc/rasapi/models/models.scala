4a2e2e93cd7acfa40d3319725822b4aef545b3de
Joe Powell <joe.powell@digital.hmrc.gov.uk>
2018-02-12 12:32:51 +0000

    RAS-769: Added trimming to nino

--- app/uk/gov/hmrc/rasapi/models/models.scala
+++ app/uk/gov/hmrc/rasapi/models/models.scala
@@ -48,7 +48,7 @@ package object models {
         json match {
           case JsString(data) => data match {
             case strValue if strValue.trim.isEmpty => JsError(Seq(JsPath() -> Seq(ValidationError(missing))))
-            case strValue if !strValue.toUpperCase.matches(ninoRegex) => JsError(Seq(JsPath() -> Seq(ValidationError(invalidFormat))))
+            case strValue if !strValue.trim.toUpperCase.matches(ninoRegex) => JsError(Seq(JsPath() -> Seq(ValidationError(invalidFormat))))
             case strValue => JsSuccess(strValue)
           }
           case _ => JsError(Seq(JsPath() -> Seq(ValidationError(invalidDataType))))
